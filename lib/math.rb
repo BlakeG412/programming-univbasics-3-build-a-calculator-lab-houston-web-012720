064cf32a20a417b9e21356d0a29ee519fb11e634
Blake Gaal <bag.blakeandrewgaal@gmail.com>
2020-01-10 07:01:12 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,14 +1,19 @@
 def addition(num1, num2)
   sum = num1+num2
   puts sum
+  sum
 end
 
 def subtraction(num1, num2)
-
+  difference = num1-num2
+  puts difference
+  difference
 end
 
 def division(num1, num2)
-
+  quotient = num1/num2
+  puts quotient
+  quotient
 end
 
 def multiplication(num1, num2)
